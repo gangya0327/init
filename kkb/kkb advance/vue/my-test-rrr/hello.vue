da25d1a37c73ef866b6846464c1c27ae8b8ed78b
gangya0327 <king9999427@163.com>
2021-07-05 03:35:43 +0800

    vue基础

---
+++ kkb/kkb advance/vue/my-test-rrr/hello.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    computed: {
+      name() {
+        return this.data 
+      }
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
